86e2775dcbb275b1f4af7a4a37c14f5dea6d261c
Rodney <rodney.amani@student.moringaschool.com>
2025-03-13 07:31:56 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(record) {
+
+    let win = record.find(game => game.result === 'W');
+    
+    
+    return win ? win.year : undefined;
+}
+const record = [
+    { year: "2015", team: "Denver Broncos", result: "W" },
+    { year: "2013", team: "Denver Broncos", result: "L" },
+    { year: "1998", team: "Denver Broncos", result: "W" },
+    { year: "1997", team: "Denver Broncos", result: "W" }
+];
+
+console.log(superbowlWin(record));
+
